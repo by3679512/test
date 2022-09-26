9a772dc1bd1ef7783f6c5b5eedfad6ca4e4430bb
linbingchuan <1435159749@qq.com>
2022-09-26 20:14:52 +0800

    fisrt

---
+++ src/themes/fisherman/index.tsx
@@ -0,0 +1,28 @@
+// 钓鱼佬主题
+import React from 'react';
+import { Theme } from '../interface';
+import { DefaultSoundNames, defaultSounds } from '../default';
+
+const imagesUrls = import.meta.glob('./images/*.png', {
+    import: 'default',
+    eager: true,
+});
+
+const fishes = Object.entries(imagesUrls).map(([key, value]) => ({
+    name: key.slice(9, -4),
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    content: <img src={value} alt="" />,
+}));
+
+export const fishermanTheme: Theme<DefaultSoundNames> = {
+    title: '牛了个牛',
+    name: '华农牛',
+    icons: fishes.map(({ name, content }) => ({
+        name,
+        content,
+        clickSound: 'button-click',
+        tripleSound: 'triple',
+    })),
+    sounds: defaultSounds,
+};
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+export const randomString: (len: number) => string = (len) => {
+    const pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let res = '';
+    while (len > 0) {
+        res += pool[Math.floor(pool.length * Math.random())];
+        len--;
+    }
+    return res;
+};
+
+export const waitTimeout: (timeout: number) => Promise<void> = (timeout) => {
+    return new Promise<void>((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, timeout);
+    });
+};
+
+export const parseThemePath: (url: string) => string = (url) => {
+    const urlObj = new URL(url);
+    const params = urlObj.searchParams;
+    return decodeURIComponent(params.get('theme') || '默认');
+};
